67bae47f1d968cf0500d082d896dd3ee18aef536
Armando <56686121+Armando287@users.noreply.github.com>
2024-04-09 12:01:41 -0400

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -11,8 +11,8 @@
 module.exports = {
 
 
-   clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+   clientId: '64b44d29-3967-455a-ab33-a6fa56129a3e',
+  obfuscatedClientSecret: 'U2FsdGVkX1+nYZtvTzma458ttjh8ZA4JRFX0qqZVGZbqseyYrUvNDJe9vV/OYOT7s9jnYqihtLPyomHJbYZVNg==',
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth.
   // If you are an E5 developer subscriber, set them in Vercel's environment variables `CLIENT_ID` and `CLIENT_SECRET`
   // You would not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
